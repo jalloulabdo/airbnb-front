b72bbea6de5e48d322dad8cd1e8f7e83adb1e992
Jalloul Abderrahim <jalloulabdo88@gmail.com>
2024-07-24 21:32:35 +0100

    new commit

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -18,9 +18,10 @@ export default function IndexPage() {
             <Link key={place._id}  to={'place/'+place._id}>
               <div className="bg-gray-500 mb-2 rounded-2xl flex">
                 {place.photos?.[0] && (
+                  
                   <img
                     className="rounded-2xl object-cover aspect-square "
-                    src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                    src={"https://airbnb-api-xpe7.onrender.com/uploads/" + place.photos?.[0]}
                     alt=""
                   />
                 )}
